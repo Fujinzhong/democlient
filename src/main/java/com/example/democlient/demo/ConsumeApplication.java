1ecb150c62d19e7da13cce8a41dde92c4e1061bb
liunian110110 <liunian110110@hotmail.com>
2018-04-13 10:36:48 +0800

    init

---
+++ src/main/java/com/example/democlient/demo/ConsumeApplication.java
@@ -0,0 +1,25 @@
+package com.example.democlient.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ConsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeApplication.class, args);
+    }
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ src/main/java/com/example/democlient/demo/controller/ConsumerController.java
@@ -0,0 +1,18 @@
+package com.example.democlient.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+public class ConsumerController {
+    @Autowired
+    RestTemplate restTemplate;
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+}
